c50983a9153b4f4458fc3185791bc5df268e45c6
Arseni Zemskow <scvsh@vetki.us>
2017-12-23 19:54:09 +0300

    Bugfixes and accessibility improvements

--- js/main.js
+++ js/main.js
@@ -166,7 +166,7 @@ createRestaurantHTML = restaurant => {
     const image = document.createElement('img');
     image.className = 'restaurant-img';
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
-    image.alt = `\"${restaurant.name}\" Restaurant`;
+    image.alt = `${restaurant.name} Restaurant`;
     li.append(image);
 
     const name = document.createElement('h1');
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,7 +58,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = `\"${restaurant.name}\" Restaurant`;
+  image.alt = `${restaurant.name} Restaurant`;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
@@ -144,6 +144,7 @@ fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
+  li.setAttribute('aria-current', 'page');
   breadcrumb.appendChild(li);
 }
 
